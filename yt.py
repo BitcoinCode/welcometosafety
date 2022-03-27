ecbc4ebc2315e2f62fbe194bce7164047f03abeb
Abdulsalam Al-Hakami <abdulsalamal-hakami@Abdulsalams-MacBook-Pro-105123.local>
2022-03-28 00:40:21 +0100

    This is the first commit

---
+++ yt.py
@@ -0,0 +1,78 @@
+from typing import BinaryIO
+from urllib import response
+from xml.dom.minidom import Document
+from flask import Flask, redirect, render_template,request, send_file
+from pytube import YouTube
+from io import BytesIO
+
+app = Flask(__name__)
+
+def stream_to_buffer(self, buffer: BinaryIO) -> None:
+        """Write the media stream to buffer
+
+        :rtype: io.BytesIO buffer
+        """
+        bytes_remaining = self.filesize
+       
+
+        for chunk in request.stream(self.url):
+            # reduce the (bytes) remainder by the length of the chunk.
+            bytes_remaining -= len(chunk)
+            # send to the on_progress callback.
+            self.on_progress(chunk, buffer, bytes_remaining)
+        self.on_complete(None)
+
+
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method == "POST":
+        url = request.form["txUrl"]
+        #qs = url.split('/');
+        #print(qs[3])
+      
+        if url != '':
+            if request.form.get("highres") ==  "1":
+                yt = YouTube(url).streams.get_highest_resolution()
+            else:
+                yt = YouTube(url).streams.get_lowest_resolution()
+            
+            #print(yt.title)
+            #stream =  yt.streams.filter('mp4')
+            #stream.download()
+            #print(yt.streams)
+            #videoid = yt.video_id
+            #yt.exists_at_path,yt.title,NULL,True,NULL,NULL
+           
+            #yt.download()
+            #buffer = BytesIO();
+            #stream_to_buffer(yt,buffer)
+            buffer = BytesIO()
+            yt.stream_to_buffer(buffer)
+            buffer.seek(0)
+            return send_file(
+                buffer,
+                as_attachment=True,
+                attachment_filename = yt.default_filename ,
+                mimetype="video/mp4"
+            )
+
+            #yt.stream_to_buffer(YouTube(url).streaming_data)
+            #buffer =
+            #return send_file(
+            #    buffer,
+            #    as_attachment=True,
+            #    attachment_filename=yt.title,
+            #    mimetype="video/mp4",
+            #)
+           
+            #return render_template('index.html',test='Downloading .... ')
+    else:
+        return render_template('index.html')
+
+    
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
